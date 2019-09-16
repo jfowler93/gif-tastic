5876c29a47200c0f84efdec8e146f49ba401dba0
Josh F <fowlerjosh93@gmail.com>
2019-09-15 21:36:42 -0400

    styled page and created function to animate gifs on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var shows = ["the office", "it's always sunny in philadelphia", "parks and rec", "archer", "eastbound and down"]
+    var shows = ["The Office", "It's Always Sunny in Philadelphia", "Parks and Rec", "Archer", "Eastbound and Down", "King of the Hill"]
 
     function displayShowGif() {
         var show = $(this).attr("data-name");
@@ -21,71 +21,75 @@ $(document).ready(function () {
                 var p = $("<p>").text("Rating: " + rating);
 
                 var showImage = $("<img>");
-                showImage.attr("src", results[i].images.fixed_height.url);
+                showImage.attr("src", results[i].images.fixed_height_still.url)
+                    .attr("src", results[i].images.fixed_height_still.url)
+                    .attr('data-animate', results[i].images.fixed_height.url)
+                    .attr('data-state', "still")
+                    .addClass("showImage");
 
                 gifDiv.prepend(p);
                 gifDiv.prepend(showImage);
 
                 $("#gifArea").prepend(gifDiv);
 
-                $(gifDiv).on("click", function () {
-                    var state = $(this).attr("data-state");
-            
-                    if (state === "still") {
-                      $(this).attr("src", $(this).attr("data-animate"));
-              
-                      $(this).attr("data-state", 'animate');
-                    }
-              
-                    else{
-                      $(this).attr("src", $(this).attr("data-still"));
-                      $(this).attr("data-state", 'still');
-                    }
-                   
-                  });
+
             };
-        
+
         });
     };
-        //create a function to render buttons using array items
-        function renderButtons() {
+    //create a function to render buttons using array items
+    function renderButtons() {
 
-            // Deleting the buttons prior to adding new movies
-            // (this is necessary otherwise you will have repeat buttons)
-            $("#gifButton").empty();
+        // Deleting the buttons prior to adding new movies
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#gifButton").empty();
 
-            // Looping through the array of movies
-            for (var i = 0; i < shows.length; i++) {
+        // Looping through the array of movies
+        for (var i = 0; i < shows.length; i++) {
 
-                // Then dynamically generating buttons for each movie in the array
-                // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-                var a = $("<button>");
-                // Adding a class of movie to our button
-                a.addClass("show");
-                // Adding a data-attribute
-                a.attr("data-name", shows[i]);
-                // Providing the initial button text
-                a.text(shows[i]);
-                // Adding the button to the buttons-view div
-                $("#gifButton").append(a);
-            };
+            // Then dynamically generating buttons for each movie in the array
+            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+            var a = $("<button>");
+            // Adding a class of movie to our button
+            a.addClass("show");
+            // Adding a data-attribute
+            a.attr("data-name", shows[i]);
+            // Providing the initial button text
+            a.text(shows[i]);
+            // Adding the button to the buttons-view div
+            $("#gifButton").append(a);
         };
+    };
 
-        $("#submit-button").on("click", function (event) {
-            event.preventDefault();
+    $("#submit-button").on("click", function (event) {
+        event.preventDefault();
 
-            // This line grabs the input from the textbox
-            var newShow = $("#new-input").val().trim();
+        // This line grabs the input from the textbox
+        var newShow = $("#new-input").val().trim();
 
-            // Adding the movie from the textbox to our array
-            shows.push(newShow);
-            console.log(shows);
+        // Adding the movie from the textbox to our array
+        shows.push(newShow);
+        console.log(shows);
 
-            // Calling renderButtons which handles the processing of our movie array
-            renderButtons();
-        });
-        $(document).on("click", ".show", displayShowGif);
+        // Calling renderButtons which handles the processing of our movie array
         renderButtons();
+    });
+    $(document).on("click", ".show", displayShowGif);
+    renderButtons();
 
-        
+    $(document).on("click", ".showImage", function () {
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).data("animate"));
+
+            $(this).attr("data-state", 'animate');
+        }
+
+        else {
+            $(this).attr("src", $(this).data("still"));
+            $(this).attr("data-state", 'still');
+        }
+
+    });
 });
